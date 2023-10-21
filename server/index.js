a9653c8c170b157c496b18bd18d1ee029ef810d8
thezeeshann <info.zeeshann@gmail.com>
2023-10-21 20:07:34 +0530

    Database setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 import express from "express"
 import chalk from "chalk"
 import cors from "cors"
+import connectDB from "./config/index.js"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
+connectDB()
 
 app.use(cors())
 app.use(express.json())
+
 
 app.listen(port,()=>{
     console.log(`${chalk.green('✓')} ${chalk.blue(
