156d614d586cee705726522c35c8670bf44cf4f9
thezeeshann <info.zeeshann@gmail.com>
2023-10-28 15:59:43 +0530

    Added the routeing and update configrations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./components/core/auth/Login"
+import SignUp from "./components/core/auth/Signup";
+import Footer from "./components/common/Footer";
+import Navbar from "./components/common/Navbar";
+import Error from "./components/common/Error";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-    <h1>Hello world</h1>
+      <Navbar />
+      <main className="font-Poppins">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
