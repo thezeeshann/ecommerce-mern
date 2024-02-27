61cf69bca61a1fb5869711dd7bfc222ef09866d6
thezeeshann <info.zeeshann@gmail.com>
2024-02-27 23:44:14 +0530

    refactor: Added new routes and update css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login"
 import SignUp from "./pages/auth/SignUp";
-import Footer from "./pages/common/Footer";
-import Navbar from "./pages/common/Navbar";
+import Footer from "./components/common/Footer";
+import Navbar from "./components/common/Navbar";
 import Error from "./components/Error";
 import Shop from "./pages/shop/Shop";
 import SingleProduct from "./pages/shop/SingleProduct";
 import "./App.css";
+
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/shop" element={<Shop/>} />
-          <Route path="shop/single-product" element={<SingleProduct/>} />
+          <Route path="/shop/single-product/:productId" element={<SingleProduct/>} />
           <Route path="*" element={<Error />} />
         </Routes>
       </main>
