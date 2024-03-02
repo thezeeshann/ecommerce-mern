b02449392952dca86b558604045fe8dc8d312152
thezeeshann <info.zeeshann@gmail.com>
2024-03-02 22:10:44 +0530

    feat: Added error components

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-   <>
-   <div className="w-[80%] mx-auto p-24 mt-7 mb-10 h-[50%]">
-   <p className="text-center text-gray-500">The page you are looking for was not found.</p>
-   </div>
-   </>
-  )
-}
+    <>
+      <div className="w-[80%] mx-auto p-24 mt-7 mb-10 h-[50%] flex flex-col gap-y-2">
+        <p className="text-center text-gray-500">
+          The page you are looking for was not found.
+        </p>
+        <Link to="/">
+          <p className="text-center text-gray-500 underline cursor-pointer">
+            Back to home!
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Error
+export default Error;
