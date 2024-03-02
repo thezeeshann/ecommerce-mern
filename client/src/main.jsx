8628db3206803763908f240e019b206709c77aec
thezeeshann <info.zeeshann@gmail.com>
2024-03-02 22:17:45 +0530

    feat: setup radix ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,16 @@ import store from "./redux/store.js";
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import "./index.css";
-
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <Theme >
+          <App />
+        </Theme>
         <Toaster />
       </BrowserRouter>
     </Provider>
