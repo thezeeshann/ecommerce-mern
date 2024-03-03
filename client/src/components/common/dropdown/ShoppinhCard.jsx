c18c1a29a6a8f2a7817242173c3bfae81234376c
thezeeshann <info.zeeshann@gmail.com>
2024-03-03 19:52:53 +0530

    feat: updated shopping cart

--- client/src/components/common/dropdown/ShoppinhCard.jsx
+++ client/src/components/common/dropdown/ShoppinhCard.jsx
@@ -1,18 +1,44 @@
 import { Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
 import { RxCross1 } from "react-icons/rx";
+import { RiShoppingBagLine } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
-import {useSelector } from "react-redux"
-import { useGetSingleProductQuery } from "../../../redux/api/productApiSlice";
-
-
+import { useSelector } from "react-redux";
+import { BASE_URL, GET_SINGLE_PRODUCT_API } from "../../../redux/constant";
+import { removeToCart } from "../../../redux/features/cartSlice";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 const ShoppinhCard = ({ open, setOpen }) => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state.cart);
+  const [products, setProducts] = useState([]);
 
-  const {cart} = useSelector((state)=>state.cart)
-  const {data} = useGetSingleProductQuery(cart[0])
-  console.log(data)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const productDetails = [];
+      for (const item of cart) {
+        try {
+          const response = await axios.get(
+            `${BASE_URL}${GET_SINGLE_PRODUCT_API}/${item}`
+          );
+          const product = response.data;
+          console.log(product);
+          productDetails.push(product);
+        } catch (error) {
+          console.error(
+            `Error fetching product with ID ${item.productId}:`,
+            error
+          );
+        }
+      }
+      setProducts(productDetails);
+    };
+
+    fetchProducts();
+  }, [cart]);
 
   return (
     <>
@@ -27,12 +53,12 @@ const ShoppinhCard = ({ open, setOpen }) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-hidden">
             <div className="absolute inset-0 overflow-hidden">
-              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <div className="fixed inset-y-0 right-0 flex max-w-full pl-10 pointer-events-none">
                 <Transition.Child
                   as={Fragment}
                   enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -42,110 +68,112 @@ const ShoppinhCard = ({ open, setOpen }) => {
                   leaveFrom="translate-x-0"
                   leaveTo="translate-x-full"
                 >
-                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                      <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                        <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-lg font-medium text-gray-900">
-                            Shopping cart
-                          </Dialog.Title>
-                          <div className="ml-3 flex h-7 items-center">
-                            <button
-                              type="button"
-                              className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
-                              onClick={() => setOpen(false)}
-                            >
-                              <span className="absolute -inset-0.5" />
-                              <span className="sr-only">Close panel</span>
-                              <RxCross1
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                            </button>
+                  <Dialog.Panel className="w-screen max-w-md pointer-events-auto">
+                    <div className="flex flex-col h-full px-4 py-6 overflow-y-scroll bg-white shadow-xl">
+                      <div className="flex items-start justify-end mb-3 ">
+                        <button
+                          type="button"
+                          className="relative p-2 -m-2 text-gray-400 hover:text-gray-500"
+                          onClick={() => setOpen(false)}
+                        >
+                          <RxCross1 className="w-6 h-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                      <hr />
+
+                      {cart.length === 0 ? (
+                        <>
+                          <div className="flex flex-col items-center justify-center h-full gap-y-2">
+                            <span>
+                              {" "}
+                              <RiShoppingBagLine size={"3rem"} />
+                            </span>
+                            <p className="text-sm font-Poppins">
+                              Your shopping cart is empty
+                            </p>
                           </div>
-                        </div>
+                        </>
+                      ) : (
+                        <>
+                          {products.map((product, index) => (
+                            <Fragment key={index}>
+                              <div className="flex-1 ">
+                                <div className="flex flex-col py-2 gap-y-3">
+                                  <div className="flex flex-row items-center justify-between ">
+                                    <div className="flex flex-row items-center gap-x-2">
+                                      <img
+                                        src={product?.singleProduct?.image}
+                                        className="w-[70px] h-[70px] rounded"
+                                        alt=""
+                                      />
 
-                        <div className="mt-8">
-                          <div className="flow-root">
-                            <ul
-                              role="list"
-                              className="-my-6 divide-y divide-gray-200"
-                            >
-                              {/* {products.map((product) => ( */}
-                                <li key={data?.singleProduct?.id} className="flex py-6">
-                                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                    <img
-                                      src={data?.singleProduct?.image}
-                                      alt={data?.singleProduct?.productName}
-                                      className="h-full w-full object-cover object-center"
-                                    />
-                                  </div>
-
-                                  <div className="ml-4 flex flex-1 flex-col">
-                                    <div>
-                                      <div className="flex justify-between text-base font-medium text-gray-900">
-                                        <h3>
-                                          <p>
-                                            {data?.singleProduct?.productName}
-                                          </p>
-                                        </h3>
-                                        <p className="ml-4">{data?.singleProduct?.price}</p>
-                                      </div>
-                                      {/* <p className="mt-1 text-sm text-gray-500">
-                                        {product.color}
-                                      </p> */}
-                                    </div>
-                                    <div className="flex flex-1 items-end justify-between text-sm">
-                                      <p className="text-gray-500">
-                                        Qty {data?.singleProduct?.quantity}
+                                      <p className="font-semibold text-blue-500 ">
+                                        {product?.singleProduct?.productName.slice(
+                                          0,
+                                          10
+                                        )}
                                       </p>
-
-                                      <div className="flex">
-                                        <button
-                                          type="button"
-                                          className="font-medium text-indigo-600 hover:text-indigo-500"
-                                        >
-                                          Remove
-                                        </button>
-                                      </div>
+                                    </div>
+                                    <span
+                                      onClick={() =>
+                                        dispatch(
+                                          removeToCart(
+                                            product?.singleProduct?._id
+                                          )
+                                        )
+                                      }
+                                    >
+                                      {" "}
+                                      <MdDelete
+                                        size={"1.5rem"}
+                                        className="cursor-pointer hover:text-red-500"
+                                      />
+                                    </span>
+                                  </div>
+                                  <div className="flex flex-col">
+                                    <div className="flex flex-row items-center justify-between">
+                                      <p className="text-lg">Price</p>
+                                      <p className="text-lg">
+                                        ${product?.singleProduct?.price}
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-row items-center justify-between">
+                                      <p>Quantity</p>
+                                      <p>{product?.singleProduct?.quantity}</p>
                                     </div>
                                   </div>
-                                </li>
-                              {/* ))} */}
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
+                                </div>
+                              </div>
+                            </Fragment>
+                          ))}
 
-                      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                        <div className="flex justify-between text-base font-medium text-gray-900">
-                          <p>Subtotal</p>
-                          <p>$262.00</p>
-                        </div>
-                        <p className="mt-0.5 text-sm text-gray-500">
-                          Shipping and taxes calculated at checkout.
-                        </p>
-                        <div className="mt-6">
-                          <a
-                            href="#"
-                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                          >
-                            Checkout
-                          </a>
-                        </div>
-                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                          <p>
-                            or{" "}
-                            <button
-                              type="button"
-                              className="font-medium text-indigo-600 hover:text-indigo-500"
-                            >
-                              <Link to="/shop">Continue Shopping</Link>
-                              <span aria-hidden="true"> &rarr;</span>
-                            </button>
-                          </p>
-                        </div>
-                      </div>
+                          <div className="flex flex-col py-3 font-Poppins gap-y-5">
+                            <div className="p-5 flex flex-col bg-[#F6F7F8] gap-y-2">
+                              <div className="flex flex-row items-center justify-between ">
+                                <p className="text-sm">Free Shippling </p>
+                                <p className="text-sm">$0</p>
+                              </div>
+                              <div className="flex flex-row items-center justify-between">
+                                <p className="text-sm">Total</p>
+                                <p className="text-sm">$30</p>
+                              </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-center px-5 gap-x-3">
+                              <Link to="/shop">
+                                <button
+                                  onClick={() => setOpen(false)}
+                                  className="border-[1px] py-2 px-3 text-sm hover:bg-blue-500 hover:text-white"
+                                >
+                                  Continue Shopping
+                                </button>
+                              </Link>
+                              <button className="border-[1px] py-2 px-3 text-sm hover:bg-blue-500 hover:text-white">
+                                Proceed to Checkout
+                              </button>
+                            </div>
+                          </div>
+                        </>
+                      )}
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
