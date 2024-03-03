4e8b465444168fd9fa0dad4d70d8f7dae3a6966a
thezeeshann <info.zeeshann@gmail.com>
2024-03-03 19:51:29 +0530

    feat: Added base url api

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,8 +1,5 @@
 import { BASE_URL } from "../constant";
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-import { setUser } from "../features/profileSlice";
-import { setToken } from "../features/authSlice";
-import { toast } from "react-hot-toast";
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
@@ -10,14 +7,3 @@ export const apiSlice = createApi({
   baseQuery,
   endpoints: () => ({}),
 });
-
-export const logout = (navigate) => {
-  return (dispatch) => {
-    dispatch(setUser(null));
-    dispatch(setToken(null));
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    toast.success("Logged Out");
-    navigate("/login");
-  };
-};
