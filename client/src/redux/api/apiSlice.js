ef96586200cc9490fe46ca6906609a3e939d13aa
thezeeshann <info.zeeshann@gmail.com>
2024-03-08 23:40:31 +0530

    feat: added headers in apislice

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,7 +1,19 @@
 import { BASE_URL } from "../constant";
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ 
+  
+  baseUrl: BASE_URL,
+  prepareHeaders:(headers, { getState })=>{
+    const token = getState().auth.token;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    headers.set('Content-Type', 'application/json');
+    return headers;
+  },
+
+});
 
 export const apiSlice = createApi({
   baseQuery,
