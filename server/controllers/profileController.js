3ac6d8b4449f306c7f6e292b7de050ba98371c61
thezeeshann <info.zeeshann@gmail.com>
2024-03-10 22:54:54 +0530

    refactor: updated profile routes & models

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -3,6 +3,7 @@ import UserModel from "../models/user.js";
 
 export const updateProfile = async (req, res) => {
   try {
+    console.log("1")
     const userId = req.existUser.userId;
     const { address, city, state, country, zipCode } = req.body;
     if (!address || !city || !state || !country || !zipCode) {
@@ -11,24 +12,25 @@ export const updateProfile = async (req, res) => {
         message: "All fields are required",
       });
     }
-    console.log("4");
+    console.log("2")
     const userDetails = await UserModel.findById(userId);
     const profile = await ProfileModel.findById(userDetails.additionalDetails);
+    console.log(profile)
     if (!userDetails) {
       return res.status(404).json({
         success: false,
         message: "User does not exist",
       });
     }
-
+    console.log("4")
     profile.address = address;
     profile.city = city;
     profile.state = state;
     profile.country = country;
     profile.zipCode = zipCode;
-
+    profile.status = true
     await profile.save();
-
+    console.log("6")
     const updatedUserDetails = await UserModel.findById(userId)
       .populate("additionalDetails")
       .exec();
@@ -50,6 +52,12 @@ export const updateUsername = async (req, res) => {
   try {
     const userId = req.existUser.userId;
     const { firstName, lastName } = req.body;
+    if(!firstName || !lastName){
+      return res.status(404).json({
+        success:false,
+        message:"Enter you firstname and lastname"
+      })
+    }
     const user = await UserModel.findByIdAndUpdate(
       userId,
       { firstName, lastName },
--- server/models/profile.js
+++ server/models/profile.js
@@ -1,31 +1,36 @@
 import mongoose from "mongoose";
 
-const ProfileSchema = new mongoose.Schema({
-  address: {
-    type: String,
+const ProfileSchema = new mongoose.Schema(
+  {
+    address: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    zipCode: {
+      type: String,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+    updated: Date,
+    created: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  city: {
-    type: String,
-  },
-  state: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  zipCode: {
-    type: String,
-  },
-  updated: Date,
-  created: {
-    type: Date,
-    default: Date.now,
-  },
-},{
-    timestamps:true
-}
+  {
+    timestamps: true,
+  }
 );
 
-
-const ProfileModel = mongoose.model("Profile",ProfileSchema)
-export default ProfileModel
+const ProfileModel = mongoose.model("Profile", ProfileSchema);
+export default ProfileModel;
