2a4f24c1a97357290accf593089f38b8684010bd
thezeeshann <info.zeeshann@gmail.com>
2024-05-13 12:45:39 +0530

    refactor: update models schema

--- server/models/category.js
+++ server/models/category.js
@@ -1,18 +1,27 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-    categoryName:{
-        type:String,
-        required:true
+  categoryName: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    slug: "categoryName",
+    required: true,
+    unique: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
     },
-    slug:{
-        type:String,
-        required:true,
-        unique: true
-    },
+  ],
+});
 
-})
-
-
-const CategoryModel = mongoose.model("Category",CategorySchema)
-export default CategoryModel
+const CategoryModel = mongoose.model("Category", CategorySchema);
+export default CategoryModel;
