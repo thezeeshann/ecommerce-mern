c1c03052cd29f104230345c7919a9eedb6f3dd67
thezeeshann <info.zeeshann@gmail.com>
2024-06-02 23:35:40 +0530

    feat: Added cart api routes

--- client/src/redux/constant.js
+++ client/src/redux/constant.js
@@ -3,12 +3,10 @@ export const BASE_URL = import.meta.env.VITE_APP_BASE_URL
 export const SIGNUP_API = "/auth/register"
 export const LOGIN_API = "/auth/login"
 export const CHANGE_PASSWORD_API = "/auth/changePassword"
-
 export const UPDATE_PROFILE_API= "profile/updateProfile"
 export const UPDATE_USERNAME_API = "profile/updateUsername"
 export const GET_SINGLE_USER_API = "profile/getSingleUser"
-
-export const PRODUCT_URL = "/products"
+export const PRODUCT_URL_API = "/products"
 export const GET_SINGLE_PRODUCT_API = "/products"
-
-export const GET_REVIEW = "/review"
+export const GET_REVIEW_API = "/review"
+export const ADD_TO_CART_API = "/cart"
