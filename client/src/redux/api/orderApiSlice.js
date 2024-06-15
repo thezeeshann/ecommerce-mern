14d181d48c6186e422b55eeadbf391e4c4af180e
thezeeshann <info.zeeshann@gmail.com>
2024-06-15 22:41:11 +0530

    feat: Added oders api routes

--- client/src/redux/api/orderApiSlice.js
+++ client/src/redux/api/orderApiSlice.js
@@ -1,11 +1,22 @@
 import { apiSlice } from "./apiSlice";
-import { CREATE_ORDER_API, GET_ORDER_API } from "../constant";
+import {
+  CREATE_ORDER_API,
+  GET_ORDER_API,
+  DELETE_ORDER_API,
+  GET_SINGLE_ORDER_API,
+} from "../constant";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getOrders: builder.query({
       query: () => ({
         url: `${GET_ORDER_API}`,
+      }),
+    }),
+
+    getSingleOrder: builder.query({
+      query: (orderId) => ({
+        url: `${GET_SINGLE_ORDER_API}/${orderId}`,
       }),
     }),
 
@@ -16,7 +27,19 @@ export const orderApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+
+    deleteOrder: builder.mutation({
+      query: (orderId) => ({
+        url: `${DELETE_ORDER_API}/${orderId}`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetOrdersQuery } = orderApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetOrdersQuery,
+  useGetSingleOrderQuery,
+  useDeleteOrderMutation,
+} = orderApiSlice;
