7447487f1f44f34b7ba415453976d009753000cb
thezeeshann <info.zeeshann@gmail.com>
2024-06-19 22:06:13 +0530

    feat: Added logout hook

---
+++ client/src/hooks/useLogout.jsx
@@ -0,0 +1,19 @@
+import { setUser } from "../redux/features/profileSlice";
+import { setToken } from "../redux/features/authSlice";
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export function useLogout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    dispatch(setUser(null));
+    dispatch(setToken(null));
+    toast.success("Logged Out");
+    navigate("/login");
+  };
+
+  return logout;
+}
