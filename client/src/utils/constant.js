d4fe04782236af7e47869bfe4eb7c275cf05ed32
thezeeshann <info.zeeshann@gmail.com>
2024-06-24 23:31:09 +0530

    refactor: Added account type & cart status

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,12 @@
 export const ACCOUNT_TYPE = {
   USER: "User",
-  ADMIN: "Admin"
-}
+  ADMIN: "Admin",
+};
+
+export const CART_ITEM_STATUS = {
+  Not_processed: "Not processed",
+  Processing: "Processing",
+  Shipped: "Shipped",
+  Delivered: "Delivered",
+  Cancelled: "Cancelled",
+};
--- server/utils/constant.js
+++ server/utils/constant.js
@@ -1,7 +1,7 @@
 export const CART_ITEM_STATUS = {
+  Not_processed: "Not processed",
   Processing: "Processing",
   Shipped: "Shipped",
   Delivered: "Delivered",
   Cancelled: "Cancelled",
-  Not_processed: "Not processed",
 };
