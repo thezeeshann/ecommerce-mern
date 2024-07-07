3bcccfa8e41f835de3681786b65f6e8312bb4bf0
thezeeshann <info.zeeshann@gmail.com>
2024-07-08 00:27:20 +0530

    feat: Added pagination component

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,40 +1,47 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-
-const ProductsPagination = () => {
+const ProductsPagination = ({
+  currentPage,
+  handlePageChange,
+  totalNumberOfPages
+}) => {
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+    <div className="flex items-center justify-between">
+        <nav
+          className="inline-flex -space-x-px bg-white rounded-md shadow-sm isolate"
+        >
+          <button
+            onClick={() => handlePageChange("previous")}
+            className={`relative inline-flex items-center px-2 py-1 text-gray-400 rounded-l-md ring-1 ring-inset ring-gray-300  ${
+              currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={currentPage === 1}
+          >
+            <ChevronLeftIcon aria-hidden="true" className="w-4 h-4" />
+            <span className="text-sm">Previous</span>
+          </button>
+
+          <span
+            aria-current="page"
+            className="relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold text-white bg-blue-500"
+          >
+            {currentPage} 
+          </span>
+
+          <button
+            onClick={() => handlePageChange("next")}
+            className={`relative inline-flex items-center px-2 py-1 text-gray-400 rounded-r-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${
+              currentPage === totalNumberOfPages
+                ? "opacity-50 cursor-not-allowed"
+                : ""
+            }`}
+            disabled={currentPage === totalNumberOfPages}
+          >
+            <span className="text-sm">Next</span>
+            <ChevronRightIcon aria-hidden="true" className="w-4 h-4" />
+          </button>
+        </nav>
+    </div>
   );
 };
 
